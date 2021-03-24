d55f9fe4c837783c7edb5f7c428c4a3282effb59
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-24 01:50:17 +0000

    Deploy sohucw/mo-ui to github.com/sohucw/mo-ui.git:gh-pages

---
+++ precache-manifest.2c6e59829236e68544c0f82c774c11f9.js
@@ -0,0 +1,23 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff60f642867e1598dbc70333604a8109",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.c4aa18700273082f6454.bundle.js"
+  },
+  {
+    "revision": "def079baf81bee2aaa78",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.c4aa18700273082f6454.bundle.js"
+  },
+  {
+    "url": "vendors~main.c4aa18700273082f6454.bundle.js"
+  },
+  {
+    "revision": "a5de111222c8e718944e",
+    "url": "vendors~main.css"
+  }
+]);
